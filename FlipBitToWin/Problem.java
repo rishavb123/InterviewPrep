2751a85b5143fb118e362b2a16b91af5b35b9b3b
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-11-07 19:32:24 -0500

    removed hello temp file

---
+++ FlipBitToWin/Problem.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Problem {
+
+    public static int flipBitToWin(int n) {
+        int cur = 0;
+        boolean open = true;
+        int max = 0;
+
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println(flipBitToWin(1775));
+    }
+    
+}
